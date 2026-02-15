3b92433dbb45239088bda4572a4b411cdc2be016
Степан <144131544+BrandonSmall4512@users.noreply.github.com>
2026-02-15 10:50:45 +0300

    Update interpolation_kub_spline.py

--- interpolation_kub_spline.py
+++ interpolation_kub_spline.py
@@ -8,7 +8,6 @@ def cubic_spline(x, y):
     for i in range(1, n - 1):
         alpha[i] = 3 / h[i] * (y[i + 1] - y[i]) - 3 / h[i - 1] * (y[i] - y[i - 1])
 
-    #переменные для рассчета коэффициентов
     l = np.ones(n)
     mu = np.zeros(n)
     z = np.zeros(n)
@@ -33,19 +32,17 @@ def evaluate_spline(x, a, b, c, d, xi):
     h = xi - x[idx]
     return a[idx] + b[idx] * h + c[idx] * h**2 + d[idx] * h**3
 
-# Генерация случайных точек
 np.random.seed(52)
 num_points = 10
 x_data = np.linspace(0, 5, num_points)
 y_data = np.cos(x_data)
 b, c, d = cubic_spline(x_data, y_data)
 
-# Генерация кривой сплайна гладкая сетка для значений и рассчет примерных y
 x_spline = np.linspace(min(x_data), max(x_data), 10000)
 y_spline = [evaluate_spline(x_data, y_data, b, c, d, xi) for xi in x_spline]
 
-# Визуализация
 plt.scatter(x_data, y_data, label='Исходные точки')
 plt.plot(x_spline, y_spline, label='Кубический сплайн', color='red')
 plt.legend()
+
 plt.show()
